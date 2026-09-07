905f0460605d7990eb104bb45a12368d7d347ef3
elijah wright <elijah.wright@gmail.com>
2026-09-07 12:02:13 -0500

    Update module exports and package.json for npm-compatible packages

---
+++ index.js
@@ -0,0 +1,9 @@
+export { default as DashboardLayout } from './app/layout.tsx';
+export { default as DashboardPage } from './app/page.tsx';
+export * from './components/intelligence';
+export * from './components/layout';
+export * from './components/map';
+export * from './components/modals';
+export * from './components/popups';
+export * from './components/sidebars';
+export * from './components/civic-visualizations';
